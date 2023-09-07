5db54397391771b9c896a9fd369ad5cedf87530f
Grigorii Konstantinov <gkonstan@c3r6p3.42vienna.com>
2023-09-07 13:10:53 +0200

    bug fix with install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,7 @@ cd $HOME
 
 git clone git@github.com:MrGrif0n/tester.git Grifon_tester
 
-cd $HOME
-
 chmod +x $HOME/Grifon_tester/tester.sh
-
-RC_FILE=$HOME/.zshrc
 
 if [[ "$SHELL" == *"bash"* ]]; then  #  works no matter path is /usr/bin/bash or /bin/bash
     RC_FILE="$HOME/.bashrc"
@@ -19,7 +15,7 @@ fi
 if ! grep "gtest=" $RC_FILE &> /dev/null; then
 	echo "gtest alias not present"
 	echo "Adding alias in file: $RC_FILE"
-	echo -e "\nalias gtest=\"bash $HOME/Grifon_tester/tester.sh\"\n" >> $RC_FILE
+	echo -e "\nalias gtest=\"$HOME/Grifon_tester/tester.sh\"\n" >> $RC_FILE
 fi
 
 exec $SHELL
